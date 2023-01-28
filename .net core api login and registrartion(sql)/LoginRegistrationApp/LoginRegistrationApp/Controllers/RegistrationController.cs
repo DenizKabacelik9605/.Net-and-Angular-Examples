a1eb2b48c2f457dd2bcf281bbba3800b0032e510
Deniz Kabaçelik <deniz.kabacelik@mctek.com.tr>
2023-01-28 10:40:45 +0300

    .Net core web api login an register methods for 3.1

---
+++ .net core api login and registrartion(sql)/LoginRegistrationApp/LoginRegistrationApp/Controllers/RegistrationController.cs
@@ -0,0 +1,72 @@
+﻿using LoginRegistrationApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LoginRegistrationApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("registration")]
+
+        public string registration(Registration registration)
+        {
+
+
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("ToysCon").ToString());
+            SqlCommand cmd = new SqlCommand("Insert Into Registration(UserName,Password,Email,IsActivate) VALUES('" + registration.UserName + "','" + registration.Password + "','" + registration.Email + "','" + registration.IsActivate + "'  )", con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i > 0)
+            {
+                return "Data inserted";
+
+            }
+            else
+            {
+                return "Error";
+            }
+
+
+
+            return "";
+        }
+
+
+
+        [HttpPost]
+        [Route("login")]
+
+        public string login(Registration registration)
+        {
+
+
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("ToysCon").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("select * from Registration where Email='"+registration.Email+"' AND Password='"+registration.Password+"' AND IsActivate=1", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+
+                return "Valid User";
+            }
+            else {
+                return "Invalid User";
+            }
+
+        }
+
+    }
+}
